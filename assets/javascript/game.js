7e9345b33a4de8cc23de15db14eeb2d53018fee3
Yogi Patel <yogesh.patel@mac.com>
2017-11-08 13:46:34 -0800

    Files

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+
+
+var alphaEl = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z']]
+
+var wordList = [
+  'tokyo', 'mumbai', 'bakersfield', 'oakland', 'osaka',
+  'london', 'auckland', 'kobe', 'kyoto', 'san francisco',
+  'los angeles', 'paris', 'hong kong', 'new delhi', 'new york', 'lusaka',
+   'chicago', 'cape town', 'rome', 'moscow', 'cairo',
+  'kabul', 'shanghai',  'santiago'
+  ];
+
+//Get key strokes
+
+  document.getElementById("change").onkeyup = function() {myFunction()};
+
+      function myfunction(){
+
+      }
+
+ //Add letter to HTML; Already guessed
+
+
+ //Add  letter to HTML; Hangman
+
+ //Number of guesses
+
+
+ //Number of wins
+
+//Number of losses
+
+
+ //Play sound and refresh
