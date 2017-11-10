1ec7c13defbe25898bede179b680fedb7b6015da
Yogi Patel <yogesh.patel@mac.com>
2017-11-10 12:52:59 -0800

    wins/losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,5 @@
 
 
-// var alphaEl = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-//         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-//         't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-// //word list
-// var wordList = [
-//   'tokyo', 'mumbai', 'bakersfield', 'oakland', 'osaka',
-//   'london', 'auckland', 'kobe', 'kyoto', 'san francisco',
-//   'los angeles', 'paris', 'hong kong', 'new delhi', 'new york', 'lusaka',
-//    'chicago', 'cape town', 'rome', 'moscow', 'cairo',
-//   'kabul', 'shanghai',  'santiago'
-//   ];
-
-// //choose random word for each game
-// var activeWord = wordList[Math.floor(Math.random() * wordList.length)];
-
-// //info that will need to be temporarily stored
-// var count = 0;
-// var guessLetter = [];
-// var fillBlank;
-
-//function to run when filling the underscore
-// function gamePlay() {
-// 	for (var i = 0; i < wordList.length; i++) {
-// 		guessLetter[i] = "_";
-// 	}
-// 	s = guessLetter.join(" ");
-// 	document.getElementById("answer").innerHTML = fillBlank;
-// };
-
-// //function to get the letter pressed by User
-
-// function keyedLetter() {
-// 	var letterGuessed = document.getElementById("letter").value;
-// 	if (keyedLetter.length > 0) { //define a guess has been made
-// 		for (var i = 0; i < activeWord.length; i++) {
-// 			if (activeWord[i] === letter) {// must be exact match
-// 				guessLetter[i] = letterGuessed; // give it assigned value
-// 				}
-// 			}
-
-// 			//define how many guess user has
-// 			count++;
-// 			document.getElementById("remain").innerHTML = "Number of guesses remaining: " + count;
-// 			document.getElementById("used").innerHTML = guessLetter.join(" ");
-// 		}
-
-// 		//state how many attempts user gets
-
-// 		if(count>12) {
-// 			document.getElementById("end").innerHTML = "Game over!";
-// 			location.reload();
-// 		}
-// 	}
-
-
-
 // Create a Array of Words
 var wordList = [
   'tokyo', 'mumbai', 'bakersfield', 'oakland', 'osaka',
@@ -68,28 +11,65 @@ var wordList = [
 
 // Choose Word Randomly
 var activeWord = wordList[Math.floor(Math.random() * wordList.length)];
-// console.log(activeWord);
 
-// Create underscore based on number of words
+// Create buckets to capture guesses
 var underScores = [];
-function newGame() {
+var correctWord = [];
+var wrongWord = [];
+var wins = 0;
+var losses = 0;
+var attemptsLeft = 12;
+
+var pageUnderscores = document.getElementsByClassName("underscore");
+var pageWins = document.getElementsByClassName("rightLetters");
+var pageLosses = document.getElementsByClassName("wrongLetters");
+
+var newGame = () => {
 	for (var i = 0; i < activeWord.length; i++) {
 		underScores.push("_");
 	}
-return underScores;
+	return underScores;
 }
-console.log(newGame());
 
 // Get users guess; https://stackoverflow.com/questions/1846599/how-to-find-out-what-character-key-is-pressed
-document.addEventListener('keypress', function(event) {
-	// var keycode = event.keyCode;
+document.addEventListener('keypress', (event) => {
 	var letter = String.fromCharCode(event.keyCode);
-	console.log(letter);
+	// If user is correct
+	if(activeWord.indexOf(letter) > -1) {
+	// add to correctWord
+		correctWord.push(letter);
+	// replace underscore with letter	
+		underScores[activeWord.indexOf(letter)] = letter;
+		pageUnderscores[0].innerHTML = underScores.join(" ");
+		pageWins[0].innerHTML = correctWord;
+	// Check if letter matches guesses
+		if(underScores.join("") == activeWord) {
+			prompt('you win!');
+			resetGame();
+		}
+	}
+	else {
+		wrongWord.push(letter);
+		pageLosses[0].innerHTML = wrongWord;
+}
 });
 
-// Check if guess is right
-// if right put to right list
-//if wong put to wrong list
+ pageUnderscores[0].innerHTML = newGame().join("");
+
+ function message() {
+	document.getElementById("message").innerHTML = "Press any key to continue."
+}
+
+function resetGame() {
+   underScores = [];
+   correctWord = [];
+   wrongWord = [];
+   attemptsLeft = 12;
+   newGame();
+
+};
+
+ 
 
 
 
